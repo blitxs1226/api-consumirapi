30a43ad20479142417a264f127592f7c63c659df
blitxs1226 <javier_minches@hotmail.com>
2020-10-19 18:38:57 -0600

    primer

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+
+        <div>
+            <footer>
+
+               Mario Minches
+
+             </footer>
+
+        </div>
+
+</template>
+<style  scoped>
+footer {
+    position: fixed;
+    height: 100px;
+    bottom: 0;
+    width: 100%;
+    background-color: black;
+    color: blanchedalmond;
+}
+</style>
